c66bde44459ab2aec8c441f8739583eb6143010e
gjchen <gjchen.tw@gmail.com>
2020-12-19 11:29:08 +0800

    add astilectron

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gjchentw/skeleton-go-astilectron-from-vue
+
+go 1.15
+
+require github.com/asticode/go-astilectron v0.20.0 // indirect
---
+++ src/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/asticode/go-astikit"
+	"github.com/asticode/go-astilectron"
+)
+
+func main() {
+	port := new(int)
+
+	if len(os.Args) > 1 {
+		p, err := strconv.Atoi(os.Args[1])
+		if err != nil {
+			println(os.Args)
+			panic(err)
+		}
+		port = &p
+	}
+	// Set logger
+	l := log.New(log.Writer(), log.Prefix(), log.Flags())
+
+	// Create astilectron
+	a, err := astilectron.New(l, astilectron.Options{
+		AppName:           "Test",
+		BaseDirectoryPath: "example",
+		SkipSetup: true,
+		TCPPort: port,
+	})
+	if err != nil {
+		l.Fatal(fmt.Errorf("main: creating astilectron failed: %w", err))
+	}
+	defer a.Close()
+
+	// Handle signals
+	a.HandleSignals()
+
+	// Start
+	if err = a.Start(); err != nil {
+		l.Fatal(fmt.Errorf("main: starting astilectron failed: %w", err))
+	}
+
+	// New window
+	var w *astilectron.Window
+	if w, err = a.NewWindow("index.html", &astilectron.WindowOptions{
+		Center: astikit.BoolPtr(true),
+		Height: astikit.IntPtr(700),
+		Width:  astikit.IntPtr(700),
+	}); err != nil {
+		l.Fatal(fmt.Errorf("main: new window failed: %w", err))
+	}
+
+	// Create windows
+	if err = w.Create(); err != nil {
+		l.Fatal(fmt.Errorf("main: creating window failed: %w", err))
+	}
+
+	// Blocking pattern
+	a.Wait()
+}
